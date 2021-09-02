d83f164e9a4df9a7d53dd94e255bdf053063a734
dqwdd <dqwdd@naver.com>
2021-09-02 12:43:05 +0900

    기능별 메소드 분리, 잘못된 입력 대처
    Thread.sleep()으로 번호 딴거 누르면 2초간 정지시킴

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -3,39 +3,62 @@ package codes;
 import java.util.Scanner;
 
 public class MainDrive {
-public static void main(String[] args) {
-	
+	public static void main(String[] args) {
+
 //	메뉴를 띄우고 -> 입력 받고 -> 종료까지를 별도 함수로 분리 
 
-	printMenu();
-}
-
-
-
-
-static void printMenu() {
-//	0번을 입력할 때까지 메뉴를 계속 출력
-	
-//	입력 : Scanner
-	Scanner myScanner = new Scanner(System.in);
-	
-	while(true) {
-
-		System.out.println("======== 전화번호부 ========");
-		System.out.println("1. 전화번호 추가 등록");
-		System.out.println("2. 전화번호 목록 조회");
-		System.out.println("0. 프로그램 종료");
-		System.out.println("=========================");
-		System.out.println("메뉴 입력 : ");
-		
-		int inputMenu = myScanner.nextInt();
-		
-		if(inputMenu==0)//입력한 숫자가 0이면 반복 탈출
-			break;
-		
+		printMenu();
 	}
-	
-	System.out.println("프로그램을 종료합니다.");
-}
+
+	static void printMenu() {
+//	0번을 입력할 때까지 메뉴를 계속 출력
+
+//	입력 : Scanner
+		Scanner myScanner = new Scanner(System.in);
+
+		while (true) {
+
+			System.out.println("======== 전화번호부 ========");
+			System.out.println("1. 전화번호 추가 등록");
+			System.out.println("2. 전화번호 목록 조회");
+			System.out.println("0. 프로그램 종료");
+			System.out.println("=========================");
+			System.out.println("메뉴 입력 : ");
+
+			int inputMenu = myScanner.nextInt();
+
+			if (inputMenu == 0)// 입력한 숫자가 0이면 반복 탈출
+				break;
+			else if (inputMenu == 1)
+				addPhoneNum();
+			else if (inputMenu == 2)
+				showAllPhoneNum();
+			else {
+				System.out.println("잘못된 입력입니다. 메뉴로 돌아갑니다.");
+
+//			2초 정도 프로그램을 정지시키고 그 후 메뉴로 가는 법
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+//		1: 추가 등록 기능, 2: 목록 조회 기능, 그 외 : 잘못된 입력
+
+		}
+
+		System.out.println("프로그램을 종료합니다.");
+	}
+
+//폰번 추가하는 관련 코드 함수
+	static void addPhoneNum() {
+
+	}
+
+//모든 목록 조회하는 함수
+	static void showAllPhoneNum() {
+
+	}
 
 }
